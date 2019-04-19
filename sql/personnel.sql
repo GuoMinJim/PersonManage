4fdac4d4130f8acb00436dbb12ac5493ca8427b7
jinguomin <1291144341@qq.com>
2019-04-19 17:09:49 +0800

    增加数据库sql

---
+++ sql/personnel.sql
@@ -0,0 +1,118 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : aliyun
+Source Server Version : 50641
+Source Host           : 47.106.248.229:3306
+Source Database       : personnel
+
+Target Server Type    : MYSQL
+Target Server Version : 50641
+File Encoding         : 65001
+
+Date: 2019-04-19 17:08:44
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for dept
+-- ----------------------------
+DROP TABLE IF EXISTS `dept`;
+CREATE TABLE `dept` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `remark` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for document
+-- ----------------------------
+DROP TABLE IF EXISTS `document`;
+CREATE TABLE `document` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) DEFAULT NULL,
+  `filename` varchar(300) DEFAULT NULL,
+  `remark` varchar(300) DEFAULT NULL,
+  `path` varchar(50) DEFAULT NULL,
+  `create_date` varchar(20) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for employee
+-- ----------------------------
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dept_id` int(11) DEFAULT NULL,
+  `job_id` int(11) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `card_id` varchar(16) DEFAULT NULL,
+  `cardId` varchar(20) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `phone` varchar(11) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `sex` varchar(11) DEFAULT NULL,
+  `party` varchar(10) DEFAULT NULL,
+  `education` varchar(10) DEFAULT NULL,
+  `remark` varchar(500) DEFAULT NULL,
+  `createDate` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for job
+-- ----------------------------
+DROP TABLE IF EXISTS `job`;
+CREATE TABLE `job` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dept_id` int(11) DEFAULT NULL COMMENT '部门id',
+  `name` varchar(50) DEFAULT NULL,
+  `remark` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for notice
+-- ----------------------------
+DROP TABLE IF EXISTS `notice`;
+CREATE TABLE `notice` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) DEFAULT NULL,
+  `content` text,
+  `createDate` char(20) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for recruit
+-- ----------------------------
+DROP TABLE IF EXISTS `recruit`;
+CREATE TABLE `recruit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dept_id` int(11) DEFAULT NULL,
+  `job_id` int(11) DEFAULT NULL,
+  `person_num` int(11) DEFAULT NULL,
+  `remark` text CHARACTER SET utf8,
+  `status` varchar(255) DEFAULT NULL COMMENT '审核状态',
+  `create_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `loginname` varchar(20) DEFAULT NULL,
+  `password` char(32) DEFAULT NULL,
+  `level` varchar(4) DEFAULT NULL COMMENT '级别',
+  `createdate` char(50) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
